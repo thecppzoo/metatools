f1f7679fbcbfa9dacb4a06e409f74ffa0f577efc
thecppzoo <zwischenzug@gmail.com>
2017-08-12 17:26:40 -0700

    Update Indices.h

--- inc/meta/Indices.h
+++ inc/meta/Indices.h
@@ -1,13 +1,19 @@
+#pragma once
+
+#include "meta/IndexPack.h"
+
 namespace meta {
 
 template<unsigned long count, unsigned long... indices>
-struct Indices {
-    using type = typename Indices<count - 1, count, indices...>::type;
+struct Indices_impl {
+    using type = typename Indices_impl<count - 1, count, indices...>::type;
 };
 
 template<unsigned long... indices>
-struct Indices<0, indices...> {
+struct Indices_impl<0, indices...> {
     using type = IndexPack<unsigned long, 0, indices...>;
 };
+
+template<unsigned long count> Indices = typename Indices_impl<unsigned long, count>::type;
 
 }
